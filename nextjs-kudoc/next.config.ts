1e849132d942ac8f78b5d4516fd881a757ca4088
lesley <lesleybanadzem2017@gmail.com>
2025-04-21 16:28:37 -0500

    Replace favicon for nextjs app

--- nextjs-kudoc/next.config.ts
+++ nextjs-kudoc/next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
 		unoptimized: true, // This is required for static exports when using next/image
 		domains: ["raw.githubusercontent.com"], // Allow GitHub raw content for images
 	},
-	basePath: "/dynamic-site", // This will serve the app under /dynamic-site
 };
 
 export default nextConfig;
